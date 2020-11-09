17fd98c94a66ae870f89e0eeb2702f249ef0f873
Darin Hunt <darinhunt@Darins-MacBook-Pro-2.local>
2020-11-09 18:30:07 -0500

    adding initial stuff

---
+++ client.py
@@ -0,0 +1,34 @@
+from socket import *
+import os
+import tqdm
+#from encrypt import encrypt
+#from decrypt import decrypt
+
+client = socket(AF_INET, SOCK_STREAM)
+client.connect('10.0.0.26', 8080)
+
+with open("~/Programs/encryption/files/key.key") as f:
+    key = f.readline()
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
+#while True:
+filename = input("Path to file: ")
+#encrypt(filename, key)
+file_size = os.path.getsize(filename)
+client.recv(2048).decode("utf8")
+client.send(f"{filename}{SEPARATOR}{file_size}".encode())
+
+progress = tqdm.tqdm(range(file_size), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+
+with open(filename, "rb") as f:
+    for _ in progress:
+
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            break
+        client.sendall(bytes_read)
+        progress.update(len(bytes_read))
+
+#decrypt(filename, key)
+client.close()
---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+import tqdm
+import os
+
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 8080
+
+BUFFER_SIZE = 4096
+
+SEPARATOR = "<SEPARATOR>"
+
+server = socket.socket()
+server.bind(SERVER_HOST, SERVER_PORT)
+server.listen(10)
+
+client_socket, address = server.accept()
+
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+
+filename = os.path.basename(filename)
+filesize = int(filesize)
+
+progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+
+with open(filename, "wb") as f:
+    for _ in progress:
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:
+            break
+        f.write(bytes_read)
+        progress.update(len(bytes_read))
+client_socket.close()
+server.close()
